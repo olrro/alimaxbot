2f89d1d82b721d38c7ef1998ad2fd937fd3c8397
dw <61549047+olrro@users.noreply.github.com>
2021-02-23 18:10:56 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,13 +11,9 @@ if ( !isset( $_GET["api"] ) ) {
 Predis\Autoloader::register();
 
 $redis = new Predis\Client( $_ENV['REDIS_URL'] );
-$storage = @json_decode( $redis->get( 'database' ), 1 );
 
-if ( !$storage ){
-
-  $storage = [];
-  $storage['section'] = 'start';
-
+if ( !$storage = @json_decode( $redis->get( 'storage' ), 1 ) ){
+  $storage = [ 'section' => '' ];
 }
 
 $client = new Client( $_GET["api"] );
@@ -29,74 +25,54 @@ if ( isset( $update->message ) ) {
     $message_id = $client->easy->message_id;
     $text = $client->easy->text;
 
-    if ( $storage['section'] == 'create' ) {
+    switch ( 1 ) {
 
-      preg_match( '/^([0-9]+) (.*){1,500}$/iU', $text, $description );
+      case ( $storage['section'] == 'create' ):
 
-      if ( isset( $description['1'] ) AND isset( $description['2'] ) ) {
+        if ( preg_match( '/^([0-9]+) (.*){1,500}$/iU', $text, $description ) ) {
 
-        $item = [];
+          $item = [];
 
-        $item['id'] = $description['1'];
-        $item['description'] = $description['2'];
+          $item['id'] = $description['1'];
+          $item['description'] = $description['2'];
 
-        $item['url'] = 'https://aliexpress.ru/item/' . intval( $text ) . '.html';
-        $item['html'] = @file_get_contents( $item['url'] );
+          $item['url'] = 'https://aliexpress.ru/item/' . $item['id'] . '.html';
+          $item['html'] = @file_get_contents( $item['url'] );
 
-        preg_match( '/<meta property="og:image" content="(.*)"\/>/iU', $item['html'], $match );
 
-        if ( !empty( $match[1] ) ) {
-          $item['image'] = $match[1];
-        }
+          $conditions = [
+            '<meta property="og:image" content="(.*)"\/>' => 'image',
+            '"totalValidNum":(.*),' => 'reviews',
+            '"formatTradeCount":"(.*)",' => 'orders',
+            '"averageStar":"(.*)",' => 'rating',
+            '"formatedAmount":"(.*)",' => 'price'
+          ];
 
-        preg_match( '/"totalValidNum":(.*),/iU', $item['html'], $match );
 
-        if ( !empty( $match[1] ) ) {
-          $item['reviews'] = intval( $match[1] );
-        }
+          foreach ( $conditions as $regex => $name ) {
 
-        preg_match( '/"formatTradeCount":"(.*)",/iU', $item['html'], $match );
+            preg_match( "/{$regex}/iU", $item['html'], $match );
+            if ( !empty( $match[1] ) ) $item[$name] = $match[1];
 
-        if ( !empty( $match[1] ) ) {
-          $item['orders'] = intval( $match[1] );
-        }
-
-        preg_match( '/"averageStar":"(.*)",/iU', $item['html'], $match );
-
-        if ( !empty( $match[1] ) ) {
-          $item['rating'] = floatval( $match[1] );
-        }
-
-        preg_match( '/"formatedAmount":"(.*)",/iU', $item['html'], $match );
-
-        if ( !empty( $match[1] ) ) {
-          $item['price'] = $match[1];
-        }
-
-        $menu["inline_keyboard"] = [
-            [
-              [ "text" => "👍", "callback_data" => "like" ],
-              [ "text" => "👎", "callback_data" => "dislike" ],
-            ],
-            [
-              [ "text" => "Купить 🧨", "url" => "http://www.google.com/", ],
-            ],
-        ];
+          }
 
           $text = [];
 
-          $text[] = "[​​​​​​​​​​​]({$item['image']}) {$item['description']}" . PHP_EOL;
+          $text[] = "[​​​​​​​​​​​]({$item['image']}){$item['description']}" . PHP_EOL;
           $text[] = "Цена - [{$item['price']}]({$item['url']})";
           $text[] = "Рейтинг - [{$item['rating']}]({$item['url']}) оценка / [{$item['orders']}]({$item['url']}) заказа(ов)";
           $text[] = "Отзывов - [{$item['reviews']}]({$item['url']})";
 
-          $storage['message'] = implode( PHP_EOL, $text );
-          $storage['menu'] = $menu;
+          $storage['ready']['text'] = implode( PHP_EOL, $text );
+          $storage['ready']['buttons'] = [
+            [ [ "text" => "👍", "callback_data" => "like" ], [ "text" => "👎", "callback_data" => "dislike" ] ],
+            [ [ "text" => "Купить 🧨", "url" => "http://www.google.com/", ] ],
+          ];
 
           $client->sendMessage(
-            $chat_id, implode( PHP_EOL, $text ), 'markdown',
+            $chat_id, implode( PHP_EOL, $storage['ready']['text'] ), 'markdown',
             null, null, null, null, null,
-            $menu
+            $storage['ready']['buttons']
           );
 
           $client->sendMessage(
@@ -104,68 +80,79 @@ if ( isset( $update->message ) ) {
             'Так будет выглядеть пост, который будет отправлен на канал, чтобы продолжить введите команду /post, для отмены - /stop'
           );
 
-      }
-      else {
+        }
+        else {
 
-        $client->sendMessage(
-          $chat_id,
-          'Данные для поста отправлены в неправильной форме'
-        );
+          $client->sendMessage(
+            $chat_id,
+            'Данные для поста отправлены в неправильной форме'
+          );
+
+          $client->sendMessage(
+            $chat_id,
+            'Чтобы создать новый пост введите идентификатор товара на Aliexpress (https://aliexpress.ru/) и текст описания (например, 32914249002 Новое классное зарядное устройство)'
+          );
+
+        }
+
+      break;
+
+      case ( $text == '/create' ):
 
         $client->sendMessage(
           $chat_id,
           'Чтобы создать новый пост введите идентификатор товара на Aliexpress (https://aliexpress.ru/) и текст описания (например, 32914249002 Новое классное зарядное устройство)'
         );
 
-      }
+        $storage['section'] = 'create';
 
-    }
+      break;
 
-    if ( $text == '/create' ) {
+      case ( $text == '/post' ):
 
-      $client->sendMessage(
-        $chat_id,
-        'Чтобы создать новый пост введите идентификатор товара на Aliexpress (https://aliexpress.ru/) и текст описания (например, 32914249002 Новое классное зарядное устройство)'
-      );
+        if ( empty( $storage['ready'] ) ) {
 
-      $storage['section'] = 'create';
+          $client->sendMessage(
+            $chat_id,
+            'Внимание, ваш пост еще не готов. Введите команду /create, чтобы создать новую запись'
+          );
 
-    }
+        }
+        else {
 
-    if ( $text == '/post' ) {
+          $client->sendMessage( $chat_id, 'Ваш пост был успешно опубликован!' );
 
-      if ( empty( $storage['message'] ) ) {
+          $client->sendMessage(
+            '-1001432760770', $storage['ready']['text'], 'markdown',
+            null, null, null, null, null,
+            $storage['ready']['buttons']
+          );
+
+          $storage = [];
+
+        }
+
+      break;
+
+      case ( $text == '/stop' ):
 
         $client->sendMessage(
           $chat_id,
-          'Внимание, ваш пост еще не готов. Введите команду /create, чтобы создать новую запись'
-        );
-
-      }
-      else {
-
-        $client->sendMessage( $chat_id, 'Ваш пост был успешно опубликован!' );
-
-        $client->sendMessage(
-          '-1001432760770', $storage['message'], 'markdown',
-          null, null, null, null, null,
-          $storage['menu']
+          'Публикация поста была отменена, чтобы создать новый пост введите команду /create'
         );
 
         $storage = [];
 
-      }
+      break;
 
-    }
+      default:
 
-    if ( $text == '/stop' ) {
+        $client->sendMessage(
+          $chat_id,
+          'Такой команды не найдено, введите команду /help, чтобы увидеть список всех команд'
+        );
 
-      $client->sendMessage(
-        $chat_id,
-        'Публикация поста была отменена, чтобы создать новый пост введите команду /create'
-      );
-
-      $storage = [];
+      break;
 
     }
 
@@ -217,5 +204,6 @@ if ( isset( $update->callback_query ) ) {
 
 }
 
-$storage = json_encode( $storage );
-$redis->set( 'database', $storage );
+if ( $storage = @json_encode( $storage ) ) {
+  $redis->set( 'storage', @json_encode( $storage ) );
+}
